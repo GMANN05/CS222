6ac0f06499bd3177bf403eaf15d3890e650ee75a
Grant Marcum <grantly05@outlook.com>
2024-08-29 11:42:21 -0400

    List of strings and more list functions

---
+++ listOfStrings.py
@@ -0,0 +1,7 @@
+def main():
+    students = ["Alice", "Bob", "Carol", "Dave", "Eve"]
+    print(len(students)) 
+    for s in students:
+        for c in s:
+            print(c)
+main()
---
+++ lists.py
@@ -0,0 +1,14 @@
+def main():
+    midterm = [90, 80, 100, 75, 82]
+    print(midterm[1])
+    for m in midterm:
+        print(m)
+    print(len(midterm))
+    print(max(midterm))
+    midterm.append(93)
+    midterm.insert(2, 70)
+    print(midterm)
+    midterm.remove(80)
+    print(midterm)
+    
+main()
